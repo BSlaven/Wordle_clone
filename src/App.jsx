0745506b0debfaaa3ef4324b15f4386447e7f663
Slaven B <slaven.bunijevac@gmail.com>
2022-04-21 23:57:26 +0200

    output fetched solution to the page as sanity check

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,19 @@ function App() {
 
   const [ solution, setSolution ] = useState(null);
 
-
   useEffect(() => {
     fetch('http://localhost:3001/solutions')
       .then(res => res.json())
       .then(json => {
-        console.log(json);
+        const randomSolution = json[Math.floor(Math.random() * json.length)];
+        setSolution(randomSolution.word);
       });
-  }, []);
+  }, [setSolution]);
 
   return (
     <div className="App">
       <h1>Wordle</h1>
+      <p>Solution is: {solution}</p>
     </div>
   )
 }
