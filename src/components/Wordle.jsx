fa19741e8a9beaf3140b8e6c0f176b7850ad959d
Slaven B <slaven.bunijevac@gmail.com>
2022-04-22 12:58:33 +0200

    add window listener for user typing

--- src/components/Wordle.jsx
+++ src/components/Wordle.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+import useWordle from "../hooks/useWordle";
+
 const Wordle = ({ solution }) => {
+
+  const { currentGuess, handleKeyup } = useWordle(solution);
+
+  useEffect(() => {
+    window.addEventListener('keyup', handleKeyup);
+
+    return () => window.removeEventListener('keyup', handleKeyup);
+  }, [handleKeyup]);
+  
   return (
     <div>Wordle</div>
   )
