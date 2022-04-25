b9b510b3a49bde167b07ff9c379f9ad540c1cfab
Slaven B <slaven.bunijevac@gmail.com>
2022-04-25 22:33:19 +0200

    prevent submission of word under certain conditions

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -1,15 +1,17 @@
-import { useInsertionEffect, useState } from 'react';
+import { useState } from 'react';
 
 const useWordle = (solution) => {
   const [ turn, setTurn ] = useState(0);
   const [ currentGuess, setCurrentGuess ] = useState('');
   const [ guesses, setGuesses ] = useState([]);
-  const [ history, setHistory ] = useState([]);
+  const [ history, setHistory ] = useState(['hello', 'ninja']);
   const [ isCorrect, setIsCorrect ] = useState(false);
 
   // format a guess into an array of letter objects
   // each object will have a key and color properties
-  const formatGuess = () => {}
+  const formatGuess = () => {
+    console.log('formatting the guess - ', currentGuess);
+  }
 
   // add new guess to state
   const addNewGuess = () => {}
@@ -17,20 +19,38 @@ const useWordle = (solution) => {
   // handle keyup event
   const handleKeyup = ({ key }) => {
 
+    if(key === 'Enter') {
+      // add guess if turn is less than 5
+      if(turn > 5) {
+        console.log('you used all your guesses')
+        return;
+      }
+      // no duplicate words
+      if(history.includes(currentGuess)) {
+        console.log('you already tried that word');
+        return;
+      }
+
+      // make sure the word is five characters long
+      if(currentGuess.length !== 5) {
+        console.log('word must be 5 characters long');
+        return;
+      }
+      formatGuess();
+    }
+
     if(key === 'Backspace') {
       setCurrentGuess(prev => prev.slice(0, -1));
       return;
     }
     
     if(/^[a-zA-Z]$/.test(key)) {
-      if(currentGuess.length <5) {
+      if(currentGuess.length < 5) {
         setCurrentGuess((prev) => {
           return prev + key
         });
       }
     }
-    console.log(key);
-    
   }
 
   return {
