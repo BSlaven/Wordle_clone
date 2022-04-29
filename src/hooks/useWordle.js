898ab4aa929457754ceac7d31fb951dd91a5e5b8
Slaven B <slaven.bunijevac@gmail.com>
2022-04-29 23:05:44 +0200

    add new guess to guesses array

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 const useWordle = (solution) => {
   const [ turn, setTurn ] = useState(0);
   const [ currentGuess, setCurrentGuess ] = useState('');
-  const [ guesses, setGuesses ] = useState([]);
+  const [ guesses, setGuesses ] = useState([...Array(6)]);
   const [ history, setHistory ] = useState(['hello', 'ninja']);
   const [ isCorrect, setIsCorrect ] = useState(false);
 
@@ -36,7 +36,16 @@ const useWordle = (solution) => {
   }
 
   // add new guess to state
-  const addNewGuess = () => {}
+  const addNewGuess = (formattedGuess) => {
+    if(currentGuess === solution) {
+      setIsCorrect(true);
+    }
+    setGuesses(prevGuesses => {
+      let newGuesses = [...prevGuesses];
+      newGuesses[turn] = formattedGuess;
+      return newGuesses;
+    });
+  }
 
   // handle keyup event
   const handleKeyup = ({ key }) => {
@@ -59,7 +68,7 @@ const useWordle = (solution) => {
         return;
       }
       const formatted = formatGuess();
-      console.log(formatted);
+      addNewGuess(formatted);
     }
 
     if(key === 'Backspace') {
