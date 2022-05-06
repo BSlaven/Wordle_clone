93ba6ea8c575528348333aa053805709009b65e6
Slaven B <slaven.bunijevac@gmail.com>
2022-05-06 13:54:36 +0200

    introduce keyboard component

---
+++ src/components/Keypad.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const Keypad = () => {
+
+  const [ letter, setLetters ] = useState(null);
+  
+  return (
+    <div>Keypad</div>
+  )
+}
+
+export default Keypad;
